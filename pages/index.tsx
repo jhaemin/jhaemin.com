1b163b53ccee02fb73275c6875ae6798a32f77a6
Jang Haemin <io@jhaemin.com>
2021-07-09 23:39:36 +0900

    Use sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
+import styles from '@/styles/Home.module.scss'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
