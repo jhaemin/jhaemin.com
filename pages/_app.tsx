f3a300a924d6787a86f09a7be94a9f222d9c97cd
Jang Haemin <io@jhaemin.com>
2021-07-09 23:30:25 +0900

    Configure web fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="/fonts.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          환영합니다 <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
