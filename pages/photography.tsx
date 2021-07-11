6f0c84e4aeccf4ddf5aa201489e617e346845928
Jang Haemin <io@jhaemin.com>
2021-07-11 15:43:59 +0900

    Create photography page

---
+++ pages/photography.tsx
@@ -0,0 +1,82 @@
+import PageInfo from '@/components/PageInfo'
+import { Page } from '@/types'
+import $ from './photography.module.scss'
+
+const photos: {
+  file: string
+  title: string
+  caption: string
+}[] = [
+  {
+    file: 'new-york-city.jpeg',
+    title: 'New York City',
+    caption: 'February 3, 2020 | iPhone 8',
+  },
+  {
+    file: '34th-st.jpeg',
+    title: '34th St',
+    caption: 'January 31th, 2020 | iPhone 8',
+  },
+  {
+    file: 'janes-carousel.jpeg',
+    title: 'Jane’s Carousel',
+    caption: 'February 2nd, 2020 | iPhone 8',
+  },
+  {
+    file: 'under-the-brooklyn-bridge.jpeg',
+    title: 'Under the Brooklyn Bridge',
+    caption: 'February 3rd, 2020 | iPhone 8',
+  },
+  {
+    file: 'gyration-july-02-2015.jpeg',
+    title: 'Gyration',
+    caption: 'July 2nd, 2015 | iPhone 5s',
+  },
+  {
+    file: 'schloss-neuschwanstein-july-03-2015.jpeg',
+    title: 'Schloss Neuschwanstein',
+    caption: 'July 3rd, 2015 | iPhone 5s',
+  },
+  {
+    file: 'grand-canal-july-04-2015.jpeg',
+    title: 'Grand Canal',
+    caption: 'July 4th, 2015 | iPhone 5s',
+  },
+  {
+    file: 'jungfrau-july-09-2015.jpeg',
+    title: 'Jungfrau',
+    caption: 'July 9th, 2015 | iPhone 5s',
+  },
+  {
+    file: 'flowerpot-may-16-2018.jpeg',
+    title: 'Flowerpot',
+    caption: 'May 16th, 2018 | iPhone 5s',
+  },
+  {
+    file: 'stairs-october-15-2019.jpeg',
+    title: 'Stairs',
+    caption: 'October 15th, 2019 | iPhone 8',
+  },
+]
+
+const Photography: Page = () => {
+  return (
+    <div className={$.photography}>
+      <PageInfo title="Photography | Jang Haemin" />
+
+      <div className={$.photosContainer}>
+        {photos.map(({ file: src, title, caption }, i) => (
+          <div key={i} className={$.photo}>
+            <img src={`/images/photography/${src}`} alt={title} />
+            <caption>
+              <h2 className={$.photoTitle}>{title}</h2>
+              <p className={$.info}>{caption}</p>
+            </caption>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Photography
