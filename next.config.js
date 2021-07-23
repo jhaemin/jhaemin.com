bc4cf8eddc52dd8fc9439f35a4f8f5f70151d272
Jang Haemin <io@jhaemin.com>
2021-07-23 22:25:06 +0900

    Import svg as a component

--- next.config.js
+++ next.config.js
@@ -8,4 +8,12 @@ module.exports = {
       },
     ]
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
