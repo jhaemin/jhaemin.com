738159d5b075b372cbe4e85ce6d4a526f2d7bed0
Jang Haemin <io@jhaemin.com>
2021-07-24 17:28:40 +0900

    Create a type of svg component

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,9 @@
 import { NextPage } from 'next'
+import { FunctionComponent, SVGProps } from 'react'
 import { LayoutName } from './layouts/LayoutWrapper'
 
 export type Page = NextPage & {
   layout?: LayoutName
 }
+
+export type SVGComponent = FunctionComponent<SVGProps<SVGElement>>
