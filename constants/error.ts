cbb0653ac850e57b5c01cbcf6e02595b2963146a
Jang Haemin <io@jhaemin.com>
2021-07-28 00:00:08 +0900

    Define errors globally

---
+++ constants/error.ts
@@ -0,0 +1,37 @@
+export type ApiError = {
+  code: string
+  msg: string
+}
+
+export const error = {
+  /** Wrong request data format */
+  REQ_001: {
+    code: 'REQ_001',
+    msg: 'Wrong request data format',
+  },
+  /** Unauthorized */
+  AUTH_000: {
+    code: 'AUTH_000',
+    msg: 'Unauthorized',
+  },
+  /** Already joined email */
+  AUTH_001: {
+    code: 'AUTH_001',
+    msg: 'Already joined email',
+  },
+  /** Not in the pending list */
+  AUTH_002: {
+    code: 'AUTH_002',
+    msg: 'Not in the pending list',
+  },
+  /** Wrong magic key */
+  AUTH_003: {
+    code: 'AUTH_003',
+    msg: 'Wrong magic key',
+  },
+  /** Wrong email format */
+  AUTH_004: {
+    code: 'AUTH_004',
+    msg: 'Wrong email format',
+  },
+}
