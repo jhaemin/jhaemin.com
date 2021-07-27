bb91075a1f9afb8eb7483c1c33852a639fcbe91d
Jang Haemin <io@jhaemin.com>
2021-07-27 23:59:59 +0900

    Create custom getServerSideProps types

---
+++ types/next.ts
@@ -0,0 +1,19 @@
+import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
+import { Session } from 'next-iron-session'
+import { ParsedUrlQuery } from 'querystring'
+
+export type JhmGetServerSidePropsContext<
+  Q extends ParsedUrlQuery = ParsedUrlQuery
+> = GetServerSidePropsContext & {
+  req: GetServerSidePropsContext['req'] & {
+    session: Session
+    userId?: number
+  }
+}
+
+export type JhmGetServerSideProps<
+  P extends { [key: string]: any } = { [key: string]: any },
+  Q extends ParsedUrlQuery = ParsedUrlQuery
+> = (
+  context: JhmGetServerSidePropsContext<Q>
+) => Promise<GetServerSidePropsResult<P>>
