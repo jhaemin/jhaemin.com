33d9453837cd1c25f72bad386dbad6d7a141cf5a
Jang Haemin <io@jhaemin.com>
2021-07-27 22:25:23 +0900

    Extract constants from page components

---
+++ constants/photos.ts
@@ -0,0 +1,67 @@
+export const photos: {
+  file: string
+  // img: StaticImageData
+  title: string
+  caption: string
+}[] = [
+  {
+    file: 'new-york-city.jpeg',
+    // img: newYorkCity,
+    title: 'New York City',
+    caption: 'February 3, 2020 | iPhone 8',
+  },
+  {
+    file: '34th-st.jpeg',
+    // img: street,
+    title: '34th St',
+    caption: 'January 31th, 2020 | iPhone 8',
+  },
+  {
+    file: 'janes-carousel.jpeg',
+    // img: janesCarousel,
+    title: 'Jane’s Carousel',
+    caption: 'February 2nd, 2020 | iPhone 8',
+  },
+  {
+    file: 'under-the-brooklyn-bridge.jpeg',
+    // img: brooklynBridge,
+    title: 'Under the Brooklyn Bridge',
+    caption: 'February 3rd, 2020 | iPhone 8',
+  },
+  {
+    file: 'gyration-july-02-2015.jpeg',
+    // img: gyration,
+    title: 'Gyration',
+    caption: 'July 2nd, 2015 | iPhone 5s',
+  },
+  {
+    file: 'schloss-neuschwanstein-july-03-2015.jpeg',
+    // img: neuschwanstein,
+    title: 'Schloss Neuschwanstein',
+    caption: 'July 3rd, 2015 | iPhone 5s',
+  },
+  {
+    file: 'grand-canal-july-04-2015.jpeg',
+    // img: grandCanal,
+    title: 'Grand Canal',
+    caption: 'July 4th, 2015 | iPhone 5s',
+  },
+  {
+    file: 'jungfrau-july-09-2015.jpeg',
+    // img: jungfrau,
+    title: 'Jungfrau',
+    caption: 'July 9th, 2015 | iPhone 5s',
+  },
+  {
+    file: 'flowerpot-may-16-2018.jpeg',
+    // img: flowerpot,
+    title: 'Flowerpot',
+    caption: 'May 16th, 2018 | iPhone 5s',
+  },
+  {
+    file: 'stairs-october-15-2019.jpeg',
+    // img: stairs,
+    title: 'Stairs',
+    caption: 'October 15th, 2019 | iPhone 8',
+  },
+]
