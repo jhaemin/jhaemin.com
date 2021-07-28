e56229f0f2f9029da32bf2dcad034cb8e4e79889
Jang Haemin <io@jhaemin.com>
2021-07-28 22:40:07 +0900

    Replace user data with just userId
    - Add name field to Page component
    - Make some of the fields of response object optional

--- types/general.ts
+++ types/general.ts
@@ -3,10 +3,10 @@ import { User } from '@prisma/client'
 import { NextApiRequest, NextApiResponse, NextPage } from 'next'
 import { Session } from 'next-iron-session'
 import { FunctionComponent, SVGProps } from 'react'
-import { ErrorCode } from './error'
 
 export type Page<P = {}> = NextPage<P> & {
   layout?: LayoutName
+  name?: string
 }
 
 export type SVGComponent = FunctionComponent<SVGProps<SVGElement>>
@@ -21,15 +21,15 @@ export enum HttpMethod {
 
 export interface JhmApiRequest extends NextApiRequest {
   session: Session
-  user: User
+  userId?: number
 }
 
-export type ResponseData = {
+export type ResponseData = null | {
   err: null | {
     code: string
     msg: string
   }
-  payload: Record<string, any>
+  payload?: Record<string, any>
 }
 
 export interface JhmApiResponse extends NextApiResponse<ResponseData> {}
