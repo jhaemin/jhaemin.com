93a85b51036acf821640649ee661ab62197399de
Jang Haemin <io@jhaemin.com>
2021-07-30 22:35:17 +0900

    Allow button component to extend its class name

--- components/ui/button/Button.tsx
+++ components/ui/button/Button.tsx
@@ -1,10 +1,14 @@
+import clsx from 'clsx'
 import { ButtonHTMLAttributes, FC } from 'react'
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
 
 const Button: FC<ButtonProps> = (props) => {
   return (
-    <button {...props} className="web-ui-input-common web-ui-button">
+    <button
+      {...props}
+      className={clsx('web-ui-input-common', 'web-ui-button', props.className)}
+    >
       {props.children}
     </button>
   )
