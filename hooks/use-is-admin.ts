3352626aac3bbecbdcc03eec1931f4d1358a4af7
Jang Haemin <io@jhaemin.com>
2021-08-07 09:57:17 +0900

    A hook for checking admin

---
+++ hooks/use-is-admin.ts
@@ -0,0 +1,11 @@
+import { IsAdminResPayload } from '@/pages/api/is-admin'
+import { ResponseData } from '@/types/general'
+import useSWR from 'swr'
+
+const useIsAdmin = () => {
+  const { data } = useSWR<ResponseData<IsAdminResPayload>>('/is-admin')
+
+  return data?.payload?.isAdmin ?? false
+}
+
+export default useIsAdmin
