b7f360878b4d3d4c2a1dd462e0421a67f75d802b
Jang Haemin <io@jhaemin.com>
2021-08-08 10:02:39 +0900

    Display new article button only for admin

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,11 +1,14 @@
 import CushionLink from '@/components/CushionLink'
+import OnlyAdmin from '@/components/OnlyAdmin'
 import PageInfo from '@/components/PageInfo'
+import Button from '@/components/ui/button/Button'
 import prisma from '@/prisma'
 import { Page } from '@/types/general'
 import { JhmGetServerSideProps } from '@/types/next'
 import { withSessionPage } from '@/utils/node/with-session'
 import { Article } from '@prisma/client'
 import dayjs from 'dayjs'
+import Link from 'next/link'
 import $ from './Articles.module.scss'
 
 type ArticlesMainPageProps = {
@@ -18,6 +21,14 @@ const ArticlesMainPage: Page<ArticlesMainPageProps> = ({ articles }) => {
       <PageInfo title="Articles | Jang Haemin" />
 
       {/* {isAdmin && <Button className={$.newButton}>New</Button>} */}
+
+      <OnlyAdmin>
+        <Link href="/articles/editor">
+          <a>
+            <Button className={$.newButton}>New</Button>
+          </a>
+        </Link>
+      </OnlyAdmin>
 
       <ol className={$.articlesList}>
         {articles.map(({ title, key, writtenAt }) => (
