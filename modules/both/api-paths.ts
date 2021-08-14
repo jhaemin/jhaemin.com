9db6a7c44b1b506ccbc691a3730cb436a3537f1a
Jang Haemin <io@jhaemin.com>
2021-08-14 19:31:51 +0900

    Generate api paths

---
+++ modules/both/api-paths.ts
@@ -0,0 +1,10 @@
+const apiPaths = {
+  authIsAdmin: 'auth/is-admin',
+  authUser: 'auth/user',
+  join: 'join',
+  signIn: 'sign-in',
+  verifyJoin: 'verify-join',
+  verifySignIn: 'verify-sign-in'
+}
+
+export default apiPaths
