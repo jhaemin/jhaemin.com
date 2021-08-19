731ec1d5c2518b5dbd6927db47415fcee383a0b6
Jang Haemin <io@jhaemin.com>
2021-08-19 21:17:48 +0900

    Create an api for fetching user information

---
+++ pages/api/auth/user.ts
@@ -0,0 +1,43 @@
+import prisma from '@/prisma'
+import { HttpMethod } from '@/types/general'
+import { makeApiHandler } from '@/utils/node/make-handler'
+import { withSessionApi } from '@/utils/node/with-session'
+import { User } from '@prisma/client'
+
+export type GetUserPayload = {
+  email: User['email']
+} | null
+
+const handler = makeApiHandler<GetUserPayload>(async (req, res) => {
+  const { userId } = req
+
+  if (!userId) {
+    console.log('here')
+    return res.json({
+      err: null,
+      payload: null,
+    })
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  })
+
+  if (!user) {
+    return res.json({
+      err: null,
+      payload: null,
+    })
+  }
+
+  res.json({
+    err: null,
+    payload: {
+      email: user.email,
+    },
+  })
+}, HttpMethod.GET)
+
+export default withSessionApi(handler)
