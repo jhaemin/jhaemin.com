6da72157649c7229d50a7cdcaf94605cfd538d5c
Jang Haemin <io@jhaemin.com>
2021-11-29 10:53:55 +0900

    Correctly scope the class names

--- pages/photography.tsx
+++ pages/photography.tsx
@@ -5,16 +5,20 @@ import $ from './photography.module.scss'
 
 const Photography: Page = () => {
   return (
-    <div className={$.photography}>
+    <div className="photography">
       <PageInfo title="Photography | Jang Haemin" />
 
-      <div className={$.photosContainer}>
+      <div className={$['photos-container']}>
         {photos.map(({ file, title, caption }, i) => (
-          <figure key={i} className={$.photo}>
-            <img src={`/images/photography/${file}`} alt={title} />
-            <figcaption>
-              <h2 className={$.photoTitle}>{title}</h2>
-              <p className={$.info}>{caption}</p>
+          <figure key={i} className={$['photo']}>
+            <img
+              className={$['image']}
+              src={`/images/photography/${file}`}
+              alt={title}
+            />
+            <figcaption className={$['caption']}>
+              <h2 className={$['photo-title']}>{title}</h2>
+              <p className={$['info']}>{caption}</p>
             </figcaption>
           </figure>
         ))}
