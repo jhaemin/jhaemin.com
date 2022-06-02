12e2b97baa86dc50bf4f6b1ed0b36f78c7421a8b
Jang Haemin <io@jhaemin.com>
2022-06-02 19:31:26 +0900

    Fix type

--- components/SignedIn.tsx
+++ components/SignedIn.tsx
@@ -1,9 +1,10 @@
 import useUser from '@/hooks/use-user'
+import { ReactNode } from 'react'
 
 /**
  * Display children components when the user is signed in.
  */
-const SignedIn: React.FC = ({ children }) => {
+const SignedIn: React.FC<{ children: ReactNode }> = ({ children }) => {
   const user = useUser()
 
   if (!user) {
