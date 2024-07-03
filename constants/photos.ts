9f78343754b777fd5544ceb376d854a5fe1f7f4f
Jang Haemin <io@jhaemin.com>
2024-07-04 00:13:51 +0900

    Update photography

--- constants/photos.ts
+++ constants/photos.ts
@@ -1,6 +1,9 @@
 import street from '@/assets/images/photography/34th-st.jpeg'
 import cat from '@/assets/images/photography/cat-2022.jpeg'
+import cat2 from '@/assets/images/photography/cat-march-23-2024.jpeg'
 import flowerpot from '@/assets/images/photography/flowerpot-may-16-2018.jpeg'
+import forest from '@/assets/images/photography/forest-june-30-2024.jpeg'
+import geullim from '@/assets/images/photography/geullim-january-30-2024.jpeg'
 import glassesOnTheTable from '@/assets/images/photography/glasses-on-the-table-march-02-2023.jpeg'
 import grandCanal from '@/assets/images/photography/grand-canal-july-04-2015.jpeg'
 import gyration from '@/assets/images/photography/gyration-july-02-2015.jpeg'
@@ -10,10 +13,12 @@ import hyeopjaeBeach from '@/assets/images/photography/hyeopjae-beach-2022.jpeg'
 import janesCarousel from '@/assets/images/photography/janes-carousel.jpeg'
 import jungfrau from '@/assets/images/photography/jungfrau-july-09-2015.jpeg'
 import newYorkCity from '@/assets/images/photography/new-york-city.jpeg'
+import path from '@/assets/images/photography/path-april-20-2024.jpeg'
 import reedOcean from '@/assets/images/photography/reed-ocean-march-05-2023.jpeg'
 import neuschwanstein from '@/assets/images/photography/schloss-neuschwanstein-july-03-2015.jpeg'
 import stairs from '@/assets/images/photography/stairs-october-15-2019.jpeg'
 import brooklynBridge from '@/assets/images/photography/under-the-brooklyn-bridge.jpeg'
+import cafeWindow from '@/assets/images/photography/window-april-17-2024.jpeg'
 
 import { StaticImageData } from 'next/image'
 
@@ -24,6 +29,30 @@ export const photos: {
   caption: string
   data: StaticImageData
 }[] = [
+  {
+    file: 'forest-june-30-2024.jpeg',
+    title: 'Forest',
+    caption: 'June 30, 2024 | iPhone 15',
+    data: forest,
+  },
+  {
+    file: 'cat-march-23-2024.jpeg',
+    title: 'Cat',
+    caption: 'March 23, 2024 | iPhone 15',
+    data: cat2,
+  },
+  {
+    file: 'path-april-20-2024.jpeg',
+    title: 'Path',
+    caption: 'April 20, 2024 | iPhone 15',
+    data: path,
+  },
+  {
+    file: 'cafe-window-april-17-2024.jpeg',
+    title: 'Window',
+    caption: 'April 17, 2024 | iPhone 15',
+    data: cafeWindow,
+  },
   {
     file: 'reed-ocean-march-05-2023.jpeg',
     title: 'Reed Ocean',
@@ -89,6 +118,20 @@ export const photos: {
     data: brooklynBridge,
   },
   {
+    file: 'stairs-october-15-2019.jpeg',
+    // img: stairs,
+    title: 'Stairs',
+    caption: 'October 15th, 2019 | iPhone 8',
+    data: stairs,
+  },
+  {
+    file: 'flowerpot-may-16-2018.jpeg',
+    // img: flowerpot,
+    title: 'Flowerpot',
+    caption: 'May 16th, 2018 | iPhone 5s',
+    data: flowerpot,
+  },
+  {
     file: 'gyration-july-02-2015.jpeg',
     // img: gyration,
     title: 'Gyration',
@@ -115,19 +158,5 @@ export const photos: {
     title: 'Jungfrau',
     caption: 'July 9th, 2015 | iPhone 5s',
     data: jungfrau,
-  },
-  {
-    file: 'flowerpot-may-16-2018.jpeg',
-    // img: flowerpot,
-    title: 'Flowerpot',
-    caption: 'May 16th, 2018 | iPhone 5s',
-    data: flowerpot,
-  },
-  {
-    file: 'stairs-october-15-2019.jpeg',
-    // img: stairs,
-    title: 'Stairs',
-    caption: 'October 15th, 2019 | iPhone 8',
-    data: stairs,
   },
 ]
